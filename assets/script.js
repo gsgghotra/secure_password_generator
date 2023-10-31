ff3f300e63347aef4c93e9b057fe9ea534b32260
Gurjeet Singh <gsgghotra@gmail.com>
2023-10-31 15:31:14 +0000

    Files added and folder structure created

---
+++ assets/script.js
@@ -0,0 +1,132 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  //This function gets an array of mixed choices
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  //let passwordOptions = getPasswordOptions();
+  //This variable has the password length and the character options
+  
+
+  //if password length is 20 charcters
+      //passwordOptions.passwordlength
+      //Create a new variable for genrating poassword
+
+  //get the length of character types (ex: 4) 
+    //passwordOptions.passwordlength
+
+  //  20/4 = 5 // Math.celi -> confirm that total character length is equal to 20 again after rounding up
+
+  // when I concat those arrays together, randomise them again
+
+
+}
+
+
+/// DONT TOUCH THE CODE BELOW
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
