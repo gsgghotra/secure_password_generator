ae97be223d71ee8a270922513a6d01747dae617c
Gurjeet Singh <gsgghotra@gmail.com>
2023-10-31 17:36:54 +0000

    Returns array of random characters

--- assets/script/script.js
+++ assets/script/script.js
@@ -146,34 +146,53 @@ function getPasswordOptions() {
   }
 }
 
-//
+//Function to get multiple random numbers from each array
 function multipleRandom(arr, num) {
-
-  const shuffled = [...arr].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, num);
+  //Sort the array 
+  const shuffled = [...arr].sort(() => 0.5 - Math.random()); //ES6+ shorthand
+  return shuffled.slice(0, num); //Slice from 0 to required numbers and return
 }
 
 
 // Function for getting a random element from an array
 function getRandom(options) {
-  //This function gets the length of password and options
-  //create an array of options
-  let arrayofOptions = [options.lowercase, options.uppercase, options.numeric, options.specialChar];
+  //This function gets the length of password and options as parameter(object)
+
     //get the length of character types (ex: 4) 
-  let countofOptions = arrayofOptions.filter(Boolean).length;
+  let countofOptions = [options.lowercase, options.uppercase, options.numeric, options.specialChar].filter(Boolean).length;
   //Divide the password length with no of character type options - //Round up if result isn't integer
-  let characterNeeded = Math.round(options.passwordLength / countofOptions);
+  let characterNeeded = Math.ceil(options.passwordLength / countofOptions);
+
+  //New Array to save answers
+  let randomHolder = [];
 
   //Get Randomisers
   if(options.lowercase){
-    console.log(multipleRandom(lowerCasedCharacters, characterNeeded));
+    //Push the random numbers into array
+    randomHolder.push(multipleRandom(lowerCasedCharacters, characterNeeded));
+  }
+  if(options.uppercase){
+    //Push the random numbers into array
+    randomHolder.push(multipleRandom(upperCasedCharacters, characterNeeded));
+  }
+  if(options.numeric){
+    //Push the random numbers into array
+    randomHolder.push(multipleRandom(numericCharacters, characterNeeded));
+  }
+  if(options.specialChar){
+    //Push the random numbers into array
+    randomHolder.push(multipleRandom(specialCharacters, characterNeeded));
   }
 
 
-  //confirm that total character length is equal to 20 again after rounding up
-    
-  // when I concat those arrays together, randomise them again
+  //Shuffle the array to generate random password
+  //also confirm that total character length is equal to 20 again after rounding up
+  let randomPassword = multipleRandom(randomHolder.flat(), options.passwordLength);
 
+
+
+  // when I concat those arrays together, randomise them again
+  return randomPassword;
 }
 
 // Function to generate password with user input
